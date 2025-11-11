53ce37044a90b344733dcee44d2a6d9c8a28d597
한진희 <0200134hjh@gmail.com>
2025-11-12 07:56:10 +0900

    Update Evaluator.hs

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,12 +1,12 @@
 module Evaluator (evalRuby) where
 import qualified Data.Map as M
 import Environment
+import Parser
 
 evalRuby :: Env -> Expr -> IO ()
-evalRuby env (IntLit n) = print n
+evalRuby _ (IntLit n) = print n
 evalRuby env (Add a b) = do
-  let IntLit x = a
-  let IntLit y = b
-  print (x + y)
-evalRuby env (Call "puts" [x]) = putStrLn (show x)
-evalRuby _ _ = putStrLn "⚠️ Unimplemented expression"
+  evalRuby env a
+  evalRuby env b
+evalRuby _ (Call "puts" [x]) = putStrLn (show x)
+evalRuby _ e = putStrLn $ "⚠️ Unimplemented: " ++ show e
